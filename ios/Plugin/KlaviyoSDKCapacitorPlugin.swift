d586aa17065e4de6dd8497eb65f08865b9fe3a8d
Michael Gregorio <gregorio.michael810@gmail.com>
2024-01-02 21:06:29 +0800

    WLW-261: prep klaviyo needed logic for push

--- ios/Plugin/KlaviyoSDKCapacitorPlugin.swift
+++ ios/Plugin/KlaviyoSDKCapacitorPlugin.swift
@@ -1,5 +1,7 @@
 import Foundation
 import Capacitor
+import KlaviyoSwift
+import UserNotifications
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -11,6 +13,7 @@ public class KlaviyoSDKCapacitorPlugin: CAPPlugin {
 
     @objc func initSDK(_ call: CAPPluginCall) {
         let value = call.getString("klaviyoKey") ?? ""
+        KlaviyoSDK().initialize(with: value)
         call.resolve([
             "result": true
         ])
@@ -18,22 +21,23 @@ public class KlaviyoSDKCapacitorPlugin: CAPPlugin {
 
     @objc func setUser(_ call: CAPPluginCall) {
         let value = call.getString("email") ?? ""
+        KlaviyoSDK().set(email: value)
         call.resolve([
             "result": true
         ])
     }
     
+    // this is not needed
     @objc func requestPushPermission(_ call: CAPPluginCall) {
-    
-      call.resolve([
-          "result": true
-      ])
+        call.resolve([
+            "result": true
+        ])
     }
 
+    // this is not needed
     @objc func setPushToken(_ call: CAPPluginCall) {
-      
       call.resolve([
-          "token": "test-token"
+          "result": true
       ])
     }
 }
